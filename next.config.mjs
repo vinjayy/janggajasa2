87449de100eeb649d0aaf23d2710ac74dfd76485
vinjay <vincentwijaya244@gmail.com>
2025-05-17 09:37:59 +0700

    update hide error

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Disable static generation for pages that might have hydration issues
+  devIndicators: false,  // <== ini yang perlu ditambahkan
   experimental: {
-    // This helps with hydration issues in some cases
     optimizeCss: false,
-    // Disable static optimization for pages with hydration issues
     workerThreads: false,
-    // Disable React server components for this project
     serverComponents: false,
   },
   eslint: {
